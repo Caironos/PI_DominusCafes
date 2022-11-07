1df25efdec142a681a32377b3cd5ee9826c7d0d5
Cairon Enrik <CAIRONOSBR@outlook.com>
2022-11-07 11:34:54 -0300

    fds esse krl

---
+++ php/pesquisa.php
@@ -0,0 +1,14 @@
+<?php 
+
+    include 'conexao.php';
+
+    $cafe = $_GET ['cafe'];
+
+    $pesquisa = "SELECT `idCafe` FROM `tbCafe`WHERE `nmCafe` LIKE '%". $cafe ."%'";
+    $query = myslq_query ( $connx , $pesquisa );
+
+    $result = mysql_fetch_assoc ( $query );
+
+    echo $result;
+
+?>
