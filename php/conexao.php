888514b10d45196da5c37d582ccad1984f7cb52f
Cairon Enrik <CAIRONOSBR@outlook.com>
2022-11-10 10:59:30 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -6,13 +6,4 @@ $db_name = 'DominusCafes';
 
 $connection = mysqli_connect($host,$user,$passwd,$db_name);
 
-
-$safe_value = mysql_real_escape_string($_POST['search']);
-
-$result = mysql_query("SELECT nmCafe FROM tbCafe WHERE `nmCafe` LIKE %$safe_value%");
- while ($row = mysql_fetch_assoc($result)) {
-echo "<div id='link' onClick='addText(\"".$row['nmCafe']."\");'>" . $row['nmCafe'] . "</div>";  
- }
-
-
 ?>
